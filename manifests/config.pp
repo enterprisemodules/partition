86b41b3af17bf7cd606c800a84f0612c5e2148ae
hajee <hajee@moretIA.com>
2014-07-17 10:48:20 +0200

    Cleanup old stuff
    Signed-off-by: hajee <hajee@moretIA.com>

--- manifests/config.pp
+++
@@ -1,6 +0,0 @@
-# == Class partition::config
-#
-# This class is called from partition
-#
-class partition::config {
-}
--- manifests/install.pp
+++
@@ -1,9 +0,0 @@
-# == Class partition::install
-#
-class partition::install {
-  include partition::params
-
-  package { $partition::params::package_name:
-    ensure => present,
-  }
-}
